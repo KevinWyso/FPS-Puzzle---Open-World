5b6cd0850b1284bce6fc2fab03492a5f036cc27f
KevinWyso <kevin.wyso@gmail.com>
2017-01-27 17:18:02 -0500

    Character Creation and movement

---
+++ FPS Puzzle - Open World/Assets/playerCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCamera : MonoBehaviour {
+
+	//Gameobject to follow
+	public Transform player;
+
+	private Transform camera;
+
+	// Use this for initialization
+	void Start () {
+		camera = gameObject.GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		camera.LookAt (player);
+	}
+}
---
+++ FPS Puzzle - Open World/Assets/playerControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControl : MonoBehaviour {
+
+	private Transform player;
+
+
+	// Use this for initialization
+	void Start () {
+		//Get the player transform
+		player = gameObject.GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.W)) {
+			player.Translate (Vector3.forward * Time.deltaTime);
+		} else if (Input.GetKey (KeyCode.S)) {
+			player.Translate (Vector3.back * Time.deltaTime);
+		} else if (Input.GetKey (KeyCode.A)) {
+			player.Translate (Vector3.left * Time.deltaTime);
+		}else if (Input.GetKey (KeyCode.D)) {
+			player.Translate (Vector3.right * Time.deltaTime);
+		}
+
+	}
+}
